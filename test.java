2d90fc70e95cedc0571f6e705ef64636ac67b883
mimangzhon <2902353109@qq.com>
2023-04-05 19:44:10 +0800

    提交文件

---
+++ test.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: test
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author: 迷茫
+ * @Create: 2023/4/5 - 19:42
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
